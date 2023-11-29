7a89e572c9734893b1b5742c3cd4706c567f67ca
Kevin Franklin <kevinmfranklin@students.abtech.edu>
2023-11-29 10:07:12 -0500

    Creates load_first_names, load_last_names, and load_valid_names functions in names-functions.php

--- functions/names-functions.php
+++ functions/names-functions.php
@@ -3,7 +3,6 @@
 function load_full_names($fileName) {
   $lineNumber = 0;
 
-// Load up the array
   $FH = fopen("$fileName", "r");
   $nextName = fgets($FH);
 
@@ -16,4 +15,31 @@ function load_full_names($fileName) {
   $nextName = fgets($FH);
   }
   return $fullNames;
+}
+
+function load_first_names($fullNames) {
+  foreach($fullNames as $fullName) {
+    $startHere = strpos($fullName, ",") + 1;
+    $firstNames[] = trim(substr($fullName, $startHere));
+  }
+  return $firstNames;
+}
+
+function load_last_names($fullNames) {
+  foreach ($fullNames as $fullName) {
+    $stopHere = strpos($fullName, ",");
+    $lastNames[] = substr($fullName, 0, $stopHere);
+  }
+  return $lastNames;
+}
+
+function load_valid_names($fullNames, $lastNames, $firstNames) {
+  for($i = 0; $i < sizeof($fullNames); $i++) {
+    if(ctype_alpha($lastNames[$i].$firstNames[$i])) {
+      $validFirstNames[$i] = $firstNames[$i];
+      $validLastNames[$i] = $lastNames[$i];
+      $validFullNames[$i] = $validLastNames[$i] . ", " . $validFirstNames[$i];
+    }
+  }
+  return $validFullNames;
 }
--- index.php
+++ index.php
@@ -3,30 +3,15 @@ include 'functions/utility-functions.php';
 include 'functions/names-functions.php';
 
 $fileName = 'names-short-list.txt';
+
 $fullNames = load_full_names($fileName);
 
+$firstNames = load_first_names($fullNames);
 
-// Get all first names
-foreach($fullNames as $fullName) {
-  $startHere = strpos($fullName, ",") + 1;
-  $firstNames[] = trim(substr($fullName, $startHere));
-}
+$lastNames = load_last_names($fullNames);
 
-// Get all last names
-foreach ($fullNames as $fullName) {
-  $stopHere = strpos($fullName, ",");
-  $lastNames[] = substr($fullName, 0, $stopHere);
-}
+$validFullNames = load_valid_names($fullNames, $lastNames, $firstNames);
 
-// Get valid names
-for($i = 0; $i < sizeof($fullNames); $i++) {
- // jam the first and last name toghether without a comma, then test for alpha-only characters
- if(ctype_alpha($lastNames[$i].$firstNames[$i])) {
-     $validFirstNames[$i] = $firstNames[$i];
-     $validLastNames[$i] = $lastNames[$i];
-     $validFullNames[$i] = $validLastNames[$i] . ", " . $validFirstNames[$i];
- }
-}
 
 // ~~~~~~~~~~~~ Display results ~~~~~~~~~~~~ //
 
