ed662a163dbf30336ec4a576bac12d0e081c6cfe
Kevin Franklin <kevinmfranklin@students.abtech.edu>
2023-11-29 09:50:54 -0500

    Creates names-functions.php. Includes new file in index.php, and creates load_full_names function

---
+++ functions/names-functions.php
@@ -0,0 +1,19 @@
+<?php 
+
+function load_full_names($fileName) {
+  $lineNumber = 0;
+
+// Load up the array
+  $FH = fopen("$fileName", "r");
+  $nextName = fgets($FH);
+
+  while(!feof($FH)) {
+    if($lineNumber % 2 == 0) {
+        $fullNames[] = trim(substr($nextName, 0, strpos($nextName, " --")));
+    }
+
+  $lineNumber++;
+  $nextName = fgets($FH);
+  }
+  return $fullNames;
+}
